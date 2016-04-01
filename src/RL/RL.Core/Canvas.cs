1f83bf93ecf0a9508b1e964d0c3d1436625cc2ed
Ivutar <ast@htc-cs.com>
2016-04-01 16:00:30 +0400

    Add RL.Core project

---
+++ src/RL/RL.Core/Canvas.cs
@@ -0,0 +1,497 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL
+{
+    public enum Color
+    {
+        Black,       // 0000 0
+        Blue,        // 0001 1
+        Green,       // 0010 2
+        Teal,        // 0011 3
+        Vinous,      // 0100 4
+        Violet,      // 0101 5
+        Brown,       // 0110 6
+        LightGray,   // 0111 7
+        DarkGray,    // 1000 8
+        LightBlue,   // 1001 9
+        LightGreen,  // 1010 A
+        Heaven,      // 1011 B
+        LightRed,    // 1100 C
+        LightViolet, // 1101 D
+        Yellow,      // 1110 E
+        White,       // 1111 F
+
+        irgb0000 = Black,
+        irgb0001 = Blue,
+        irgb0010 = Green,
+        irgb0011 = Teal,
+        irgb0100 = Vinous,
+        irgb0101 = Violet,
+        irgb0110 = Brown,
+        irgb0111 = LightGray,
+        irgb1000 = DarkGray,
+        irgb1001 = LightBlue,
+        irgb1010 = LightGreen,
+        irgb1011 = Heaven,
+        irgb1100 = LightRed,
+        irgb1101 = LightViolet,
+        irgb1110 = Yellow,
+        irgb1111 = White,
+    }
+
+    public struct CharInfo
+    {
+        public char ch;
+        public Color fore;
+        public Color back;
+
+        public CharInfo(char c)
+        {
+            ch = c;
+            fore = Color.White;
+            back = Color.Black;
+        }
+
+        public CharInfo(char c, Color f, Color b)
+        {
+            ch = c;
+            fore = f;
+            back = b;
+        }
+    }
+
+    public class Canvas
+    {
+        CharInfo[,] buffer;
+
+        public int Width
+        {
+            get
+            {
+                if (buffer == null)
+                    return 0;
+                return buffer.GetLength(0);
+            }
+            set
+            {
+                int wd = value > 1 ? value : 1;
+                Resize(wd, Height);
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                if (buffer == null)
+                    return 0;
+                return buffer.GetLength(1);
+            }
+            set
+            {
+                int hg = value > 1 ? value : 1;
+                Resize(Width, hg);
+            }
+        }
+        public CharInfo this[int x, int y]
+        {
+            get
+            {
+                if (x < 0 || y < 0 || x >= Width || y >= Height)
+                    return new CharInfo();
+                return buffer[x, y];
+            }
+            set
+            {
+                if (x < 0 || y < 0 || x >= Width || y >= Height)
+                    return;
+                buffer[x, y] = value;
+            }
+        }
+
+        public Canvas()
+        {
+            buffer = new CharInfo[1, 1];
+            Clear();
+        }
+        public Canvas(int width, int height)
+        {
+            width = width < 1 ? 1 : width;
+            height = height < 1 ? 1 : height;
+            buffer = new CharInfo[width, height];
+            Clear();
+        }
+
+        public void Clear()
+        {
+            Clear(new CharInfo(' ', Color.White, Color.Black));
+        }
+        public void Clear(CharInfo ci)
+        {
+            for (int j = 0; j < Height; j++)
+                for (int i = 0; i < Width; i++)
+                    buffer[i, j] = ci;
+        }
+        public void Resize(int width, int height)
+        {
+            width = width < 1 ? 1 : width;
+            height = height < 1 ? 1 : height;
+
+            //создать новый буфер
+            CharInfo[,] tmp = new CharInfo[width, height];
+
+            //заполнить новый буфер данными по умолчанию
+            for (int j = 0; j < height; j++)
+                for (int i = 0; i < width; i++)
+                    tmp[i, j] = new CharInfo(' ', Color.White, Color.Black);
+
+            //перенести в новый буфер данные из старого буфера
+            int wd = Math.Min(Width, width);
+            int hg = Math.Min(Height, height);
+            for (int j = 0; j < hg; j++)
+                for (int i = 0; i < wd; i++)
+                    tmp[i, j] = buffer[i, j];
+
+            //заменяем старый буфер на новый
+            buffer = tmp;
+        }
+        public void Copy(Canvas from, int to_x = 0, int to_y = 0, int? to_width = null, int? to_height = null, int from_x = 0, int from_y = 0)
+        {
+            if (from == null || to_x >= Width || to_y >= Height)
+                return;
+            if (to_width == null) to_width = Width - to_x;
+            if (to_height == null) to_height = Height - to_y;
+            if (to_x < 0)
+            {
+                from_x -= to_x;
+                to_width += to_x;
+                to_x = 0;
+            }
+            if (to_y < 0)
+            {
+                from_y -= to_y;
+                to_height += to_y;
+                to_y = 0;
+            }
+            if (to_x + to_width - 1 < 0 || to_y + to_height - 1 < 0)
+                return;
+            if (from_x >= from.Width || from_y >= from.Height)
+                return;
+            if (from_x + to_width - 1 < 0 || from_y + to_height - 1 < 0)
+                return;
+            for (int y = to_y; y < to_y + to_height; y++)
+            {
+                for (int x = to_x; x < to_x + to_width; x++)
+                {
+                    int srcx = x - to_x + from_x;
+                    int srcy = y - to_y + from_y;
+                    if (srcx >= 0 && srcy >= 0 && srcx < from.Width && srcy < from.Height)
+                        buffer[x, y] = from[srcx, srcy];
+                }
+            }
+        }
+        public static Canvas Load(string filename)
+        {
+            Canvas res = new Canvas();
+
+            if (string.IsNullOrWhiteSpace(filename) || !File.Exists(filename))
+                return res;
+
+            //загрузка
+            string[] lines = File.ReadAllLines(filename);
+
+            //размеры
+            int width;
+            int height;
+
+            if (lines.Length <= 0)
+                return res;
+
+            string[] sizes = lines[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (sizes.Length != 2)
+                return res;
+
+            int.TryParse(sizes[0], out width);
+            int.TryParse(sizes[1], out height);
+
+            if (width < 1) width = 1;
+            if (height < 1) height = 1;
+
+            res = new Canvas(width, height);
+
+            //символы
+            int row = 1;
+            for (int y = 0; y < height && row < lines.Length; y++, row++)
+            {
+                string line = lines[row];
+                for (int x = 0; x < width && x < line.Length; x++)
+                {
+                    CharInfo ci = res.buffer[x, y];
+                    ci.ch = line[x];
+                    res.buffer[x, y] = ci;
+                }
+            }
+
+            //цвет символов
+            for (int y = 0; y < height && row < lines.Length; y++, row++)
+            {
+                string line = lines[row];
+                for (int x = 0; x < width && x < line.Length; x++)
+                {
+                    CharInfo ci = res.buffer[x, y];
+                    ci.fore = Char2Color(line[x]);
+                    res.buffer[x, y] = ci;
+                }
+            }
+
+            //цвет фона
+            for (int y = 0; y < height && row < lines.Length; y++, row++)
+            {
+                string line = lines[row];
+                for (int x = 0; x < width && x < line.Length; x++)
+                {
+                    CharInfo ci = res.buffer[x, y];
+                    ci.back = Char2Color(line[x]);
+                    res.buffer[x, y] = ci;
+                }
+            }
+
+            return res;
+        }
+        public void Save(string filename)
+        {
+            List<string> lines = new List<string>();
+
+            //размеры
+            lines.Add(string.Format("{0} {1}", Width, Height));
+
+            //символы
+            for (int y = 0; y < Height; y++)
+            {
+                StringBuilder sb = new StringBuilder(Width);
+                for (int x = 0; x < Width; x++)
+                    sb.Append(buffer[x, y].ch);
+                lines.Add(sb.ToString());
+            }
+
+            //цвет символов
+            for (int y = 0; y < Height; y++)
+            {
+                StringBuilder sb = new StringBuilder(Width);
+                for (int x = 0; x < Width; x++)
+                    sb.Append(Color2Char(buffer[x, y].fore));
+                lines.Add(sb.ToString());
+            }
+
+            //цвет фона
+            for (int y = 0; y < Height; y++)
+            {
+                StringBuilder sb = new StringBuilder(Width);
+                for (int x = 0; x < Width; x++)
+                    sb.Append(Color2Char(buffer[x, y].back));
+                lines.Add(sb.ToString());
+            }
+
+            //сохранение
+            File.WriteAllLines(filename, lines.ToArray());
+        }
+        public void Write(int x, int y, string text, Color fore = Color.White, Color back = Color.Black, int? width = null, int offset = 0)
+        {
+            if (string.IsNullOrEmpty(text))
+                return;
+            if (width == null)
+                width = text.Length;
+            if (x >= Width || x + width - 1 < 0)
+                return;
+            if (y >= Height || y < 0)
+                return;
+
+            int left = x;
+            int right = Math.Min(x + text.Length - 1, Width - 1);
+
+            //индексы символов, которые подлежат выводу на экран
+            int begin = left - x + offset;
+            int end = right - x + offset;
+
+            //вывод на экран
+            for (int i = left, j = begin; i <= right && j <= end; i++, j++)
+                if (j >= 0 && j < text.Length)
+                    buffer[i, y] = new CharInfo(text[j], fore, back);
+                else
+                    buffer[i, y] = new CharInfo(' ', fore, back);
+        }
+
+        private static Color Char2Color(char c)
+        {
+            c = Char.ToUpper(c);
+
+            if (c == '0') return Color.irgb0000;
+            if (c == '1') return Color.irgb0001;
+            if (c == '2') return Color.irgb0010;
+            if (c == '3') return Color.irgb0011;
+            if (c == '4') return Color.irgb0100;
+            if (c == '5') return Color.irgb0101;
+            if (c == '6') return Color.irgb0110;
+            if (c == '7') return Color.irgb0111;
+            if (c == '8') return Color.irgb1000;
+            if (c == '9') return Color.irgb1001;
+            if (c == 'A') return Color.irgb1010;
+            if (c == 'B') return Color.irgb1011;
+            if (c == 'C') return Color.irgb1100;
+            if (c == 'D') return Color.irgb1101;
+            if (c == 'E') return Color.irgb1110;
+            if (c == 'F') return Color.irgb1111;
+
+            return Color.irgb0000; //by default
+        }
+        private static char Color2Char(Color c)
+        {
+            if (c == Color.irgb0000) return '0';
+            if (c == Color.irgb0001) return '1';
+            if (c == Color.irgb0010) return '2';
+            if (c == Color.irgb0011) return '3';
+            if (c == Color.irgb0100) return '4';
+            if (c == Color.irgb0101) return '5';
+            if (c == Color.irgb0110) return '6';
+            if (c == Color.irgb0111) return '7';
+            if (c == Color.irgb1000) return '8';
+            if (c == Color.irgb1001) return '9';
+            if (c == Color.irgb1010) return 'A';
+            if (c == Color.irgb1011) return 'B';
+            if (c == Color.irgb1100) return 'C';
+            if (c == Color.irgb1101) return 'D';
+            if (c == Color.irgb1110) return 'E';
+            if (c == Color.irgb1111) return 'F';
+
+            return '0';
+        }
+    }
+
+    public static class Util
+    {
+        public static Canvas Buffer = new Canvas(80, 25);
+
+        public static string Title
+        {
+            get
+            {
+                return Console.Title;
+            }
+            set
+            {
+                Console.Title = value;
+            }
+        }
+        public static int Width
+        {
+            get
+            {
+                return Console.BufferWidth;
+            }
+            set
+            {
+                Resize(value, Height);
+                Buffer.Resize(Width, Height);
+            }
+        }
+        public static int Height
+        {
+            get
+            {
+                return Console.BufferHeight;
+            }
+            set
+            {
+                Resize(Width, value);
+                Buffer.Resize(Width, Height);
+            }
+        }
+        public static bool CursorVisible
+        {
+            get
+            {
+                return Console.CursorVisible;
+            }
+            set
+            {
+                Console.CursorVisible = value;
+            }
+        }
+
+        static public void Sleep(int milliseconds)
+        {
+            System.Threading.Thread.Sleep(milliseconds);
+        }
+        public static void Resize(int width, int height)
+        {
+            if (width < 1) width = 1;
+            if (height < 1) height = 1;
+
+            try { Console.WindowWidth = width; }
+            catch { }
+            try { Console.WindowHeight = height; }
+            catch { }
+            try { Console.BufferWidth = width; }
+            catch { }
+            try { Console.BufferHeight = height; }
+            catch { }
+
+            try { Console.WindowWidth = width; }
+            catch { }
+            try { Console.WindowHeight = height; }
+            catch { }
+            try { Console.BufferWidth = width; }
+            catch { }
+            try { Console.BufferHeight = height; }
+            catch { }
+        }
+        public static void Swap()
+        {
+            if (Buffer == null)
+                return;
+
+            int wd = Math.Min(Util.Buffer.Width, Width);
+            int hg = Math.Min(Util.Buffer.Height, Height);
+
+            //заполняем буффер
+            WinCon.CHAR_INFO[] buf = new WinCon.CHAR_INFO[wd * hg];
+            for (int y = 0; y < hg; y++)
+                for (int x = 0; x < wd; x++)
+                {
+                    buf[y * wd + x].UnicodeChar = Util.Buffer[x, y].ch;
+                    buf[y * wd + x].Attributes =
+                        (ushort)
+                        (
+                            (byte)((byte)(Util.Buffer[x, y].back) << 4)
+                            |
+                            (byte)(Util.Buffer[x, y].fore)
+                        );
+                }
+
+            //размеры копируемого буффера
+            WinCon.COORD size;
+            size.X = (short)wd;
+            size.Y = (short)hg;
+
+            //координаты внутри буффера, с которых начинается вывод
+            WinCon.COORD coord;
+            coord.X = 0;
+            coord.Y = 0;
+
+            //координаты прямоугольника на экране, куда происходит копирование
+            WinCon.SMALL_RECT rc;
+            rc.Left = (short)0;
+            rc.Right = (short)(wd - 1);
+            rc.Top = (short)0;
+            rc.Bottom = (short)(hg - 1);
+
+            //вывод в консоль
+            IntPtr handle = WinCon.GetStdHandle((int)-11);
+            WinCon.WriteConsoleOutput(handle, buf, size, coord, ref rc);
+        }
+    }
+}
---
+++ src/RL/RL.Core/Events.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL
+{
+    public enum EventKind
+    {
+        Empty = 0, //пустое событие
+        Key,       //событие от клавиатуры
+        Mouse,     //событие от мыши
+    }
+
+    [Flags]
+    public enum Modifiers
+    {
+        RIGHT_ALT_PRESSED  = 0x0001,
+        LEFT_ALT_PRESSED   = 0x0002,
+        RIGHT_CTRL_PRESSED = 0x0004,
+        LEFT_CTRL_PRESSED  = 0x0008,
+        SHIFT_PRESSED      = 0x0010,
+        NUMLOCK_ON         = 0x0020,
+        SCROLLLOCK_ON      = 0x0040,
+        CAPSLOCK_ON        = 0x0080,
+        ENHANCED_KEY       = 0x0100,
+    }
+
+    public enum MouseEventKind
+    {
+        Move,
+        Press,
+        Release,
+    }
+
+    public struct KeyEvent
+    {
+        public ConsoleKey Key;
+        public char       KeyChar;
+        
+        public bool Press;
+        public bool Alt;
+        public bool Shift;
+        public bool Ctrl;
+        public bool Auto;
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} {1} {2} {3} {4} {5} {6}",
+                Key.ToString("F"),
+                char.IsControl(KeyChar) ? "" : "'" + KeyChar + "'",
+                Press ? "Press" : "Release",
+                Alt ? "Alt" : "",
+                Shift ? "Shift" : "",
+                Ctrl ? "Ctrl" : "",
+                Auto ? "Auto" : ""
+            );
+        }
+    }
+
+    public struct MouseEvent
+    {
+        public MouseEventKind Kind;
+        public bool Left;
+        public bool Right;
+        public int X;
+        public int Y;
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} ({1}, {2}) {3} {4}",
+                Kind.ToString("F"),
+                X, Y,
+                Left ? "Left" : "",
+                Right ? "Right" : ""
+            );
+        }
+    }
+
+    public struct Event
+    {
+        static public Event EmptyEvent;
+
+        public EventKind Kind;
+
+        public KeyEvent   Key;
+        public MouseEvent Mouse;
+
+        public bool Empty
+        {
+            get { return Kind == EventKind.Empty; }
+        }
+
+        public Event(EventKind Kind, KeyEvent Key = new KeyEvent(), MouseEvent Mouse = new MouseEvent())
+        {
+            this.Kind  = Kind;
+            this.Key   = Key;
+            this.Mouse = Mouse;
+        }
+
+        public override string ToString()
+        {
+            switch (Kind)
+            {
+                case EventKind.Empty:
+                    return "Empty";
+                case EventKind.Key:
+                    return string.Format("Key: {0}", Key);
+                case EventKind.Mouse:
+                    return string.Format("Mouse: {0}", Mouse);
+            }
+            return "";
+        }
+    }
+
+    public static class Events
+    {
+        const int  STD_INPUT_HANDLE       = -10;
+        const int  KEY_EVENT              = 0x0001;
+        const int  MOUSE_EVENT            = 0x0002;
+        const int  ENABLE_MOUSE_INPUT     = 0x0010;
+        const uint ENABLE_QUICK_EDIT_MODE = 0x0040;
+        const int  ENABLE_EXTENDED_FLAGS  = 0x0080;
+
+        static List<Event> list = new List<Event>(100);
+        static bool LeftButton = false;
+        static bool RightButton = false;
+
+        static Events ()
+        {
+            var handle = WinCon.GetStdHandle(STD_INPUT_HANDLE);
+            uint mode = 0;
+            if (!(WinCon.GetConsoleMode(handle, out mode))) { throw new System.ComponentModel.Win32Exception(); }
+            mode |= ENABLE_MOUSE_INPUT;
+            mode &= ~ENABLE_QUICK_EDIT_MODE;
+            mode |= ENABLE_EXTENDED_FLAGS;
+            if (!(WinCon.SetConsoleMode(handle, mode))) { throw new System.ComponentModel.Win32Exception(); }
+        }
+
+        public static bool HasEvent
+        {
+            get
+            {
+                ReadBufferEvents();
+                return list.Count > 0;
+            }
+        }
+
+        public static Event GetNext(bool wait = false)
+        {
+            ReadBufferEvents();
+
+            if (wait)
+            {
+                while (!HasEvent)
+                    System.Threading.Thread.Sleep(10);
+            }
+            else
+            {
+                if (!HasEvent)
+                    return Event.EmptyEvent;
+            }
+
+            Event e = list[0];
+            list.RemoveAt(0);
+            return e;
+        }
+
+        static void ReadBufferEvents()
+        {
+            IntPtr handle = WinCon.GetStdHandle(STD_INPUT_HANDLE);
+            WinCon.INPUT_RECORD[] buff = new WinCon.INPUT_RECORD[1];
+            uint count;
+
+            //считываем события из буфера до тех пор, пока не получим клавиатурное или мышиное событие
+            while (true)
+            {
+                //если буфер консольниых событий пуст, то выходим
+                WinCon.GetNumberOfConsoleInputEvents(handle, out count);
+                if (count <= 0)
+                    break;
+
+                //прочитать очередное событие из буфера
+                WinCon.ReadConsoleInput(handle, buff, (uint)buff.Length, out count);
+                if (count <= 0)
+                    break;
+                else
+                {
+                    WinCon.INPUT_RECORD ir = buff[0];
+
+                    //key
+                    if (ir.EventType == KEY_EVENT)
+                    {
+                        Event e = new Event();
+
+                        e.Kind = EventKind.Key;
+
+                        e.Key.Key = (ConsoleKey)ir.KeyEvent.wVirtualKeyCode;
+                        e.Key.KeyChar = ir.KeyEvent.UnicodeChar;
+                        e.Key.Press = ir.KeyEvent.bKeyDown;
+
+                        Modifiers m = (Modifiers)ir.KeyEvent.dwControlKeyState;
+
+                        e.Key.Alt = m.HasFlag(Modifiers.LEFT_ALT_PRESSED) || m.HasFlag(Modifiers.RIGHT_ALT_PRESSED);
+                        e.Key.Shift = m.HasFlag(Modifiers.SHIFT_PRESSED);
+                        e.Key.Ctrl = m.HasFlag(Modifiers.LEFT_CTRL_PRESSED) || m.HasFlag(Modifiers.RIGHT_CTRL_PRESSED);
+                        e.Key.Auto = false; //ir.KeyEvent.wRepeatCount; //... автонажатия клавиши
+
+                        list.Add(e);
+                    }
+
+                    //mouse
+                    if (ir.EventType == MOUSE_EVENT && ir.MouseEvent.dwEventFlags == 0 || ir.MouseEvent.dwEventFlags == 1)
+                    {
+                        //move
+                        if (ir.MouseEvent.dwEventFlags == 1)
+                        {
+                            LeftButton = (ir.MouseEvent.dwButtonState & 1) != 0;
+                            RightButton = (ir.MouseEvent.dwButtonState & 2) != 0;
+
+                            Event e = new Event();
+
+                            e.Kind = EventKind.Mouse;
+
+                            e.Mouse.Kind = MouseEventKind.Move;
+                            e.Mouse.X = ir.MouseEvent.dwMousePosition.X;
+                            e.Mouse.Y = ir.MouseEvent.dwMousePosition.Y;
+                            e.Mouse.Left = LeftButton;
+                            e.Mouse.Right = RightButton;
+
+                            list.Add(e);
+                        }
+
+                        //press/release
+                        if (ir.MouseEvent.dwEventFlags == 0)
+                        {
+                            Event e = new Event();
+
+                            e.Kind = EventKind.Mouse;
+
+                            e.Mouse.X = ir.MouseEvent.dwMousePosition.X;
+                            e.Mouse.Y = ir.MouseEvent.dwMousePosition.Y;
+
+                            //было нажатие или отжатие клавиши
+                            e.Mouse.Kind = MouseEventKind.Release;
+                            bool left = (ir.MouseEvent.dwButtonState & 1) != 0;
+                            bool right = (ir.MouseEvent.dwButtonState & 2) != 0;
+                            if (!LeftButton && left) e.Mouse.Kind = MouseEventKind.Press;
+                            if (!RightButton && right) e.Mouse.Kind = MouseEventKind.Press;
+
+                            LeftButton = left;
+                            RightButton = right;
+
+                            e.Mouse.Left = LeftButton;
+                            e.Mouse.Right = RightButton;
+
+                            list.Add(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
