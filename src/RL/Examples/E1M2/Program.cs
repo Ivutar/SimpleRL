8fc44c9545d973c7bf37ac4823d75b32b787a515
Ivutar <ast@htc-cs.com>
2016-04-01 16:16:29 +0400

    Add Example E1M2 - Simple events loop

---
+++ src/RL/Examples/E1M2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E1M2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E1M2 - Simple events loop";
+            Util.Width = 40;
+            Util.Height = 20;
+            Util.CursorVisible = false;
+
+            while (true)
+            {
+                //process input events
+                Event e = Events.GetNext(true);
+                if (e.Kind == EventKind.Key && e.Key.Key == ConsoleKey.Escape)
+                    break;
+
+                //update screen
+                Util.Buffer.Clear();
+                Util.Buffer.Write(0, 0, e.ToString());
+                Util.Swap();
+            }
+
+        }
+    }
+}
