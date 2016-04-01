ff545bf4f25705d43cba7b9650f1779d6d0238a5
Ivutar <ast@htc-cs.com>
2016-04-01 16:08:09 +0400

    Add Example E1M1 - Hello World

---
+++ src/RL/Examples/E1M1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E1M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E1M1 - Hello world";
+            Util.Width = 30;
+            Util.Height = 20;
+            Util.CursorVisible = false;
+
+            Util.Buffer.Clear();
+            Util.Buffer.Write(0, 0, "Hello world");
+            Util.Swap();
+
+            Console.ReadKey(true);
+        }
+    }
+}
