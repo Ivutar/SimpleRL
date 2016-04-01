12cd23806f6289a19f6a914d4629e5677b55b876
Ivutar <ast@htc-cs.com>
2016-04-01 17:11:19 +0400

    Add Example E1M3 - Load canvas content

---
+++ src/RL/Examples/E1M3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E1M3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E1M3 - Load canvas content";
+            Util.Width = 40;
+            Util.Height = 30;
+            Util.CursorVisible = false;
+
+            Canvas pic = Canvas.Load(@"..\..\screen.rl");
+            Util.Buffer.Clear(new CharInfo(' ', Color.LightGray, Color.DarkGray));
+            Util.Buffer.Copy(pic, 3, 3);
+            Util.Swap();
+
+            Console.ReadKey(true);
+        }
+    }
+}
