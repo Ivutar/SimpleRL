25e663070f462c3e388275ed77762f2df6a510b5
Ivutar <ast@htc-cs.com>
2016-04-25 16:00:24 +0400

    update trace method

--- src/RL/Examples/E2M4/RayTracer.cs
+++ src/RL/Examples/E2M4/RayTracer.cs
@@ -7,14 +7,7 @@ using System.Threading.Tasks;
 namespace E2M4
 {
     public static class RayTracer
-    {
-        public struct Intersect
-        {
-            public int cellx;
-            public int celly;
-            public int column;
-            public bool horizontal;
-        }
+    {        
 
         public static Intersect Trace(double x, double y, double angle, int cellsize, Func<int, int, bool> filled_cell, double dist = 1000)
         {
@@ -47,7 +40,7 @@ namespace E2M4
             //traversal algorithm
             int cx = (int)Math.Floor(x);
             int cy = (int)Math.Floor(y);
-            Intersect intersect;
+            Intersect intersect = new Intersect();
             do
             {
                 if (tMaxX < tMaxY)
@@ -74,5 +67,65 @@ namespace E2M4
 
             return intersect;
         }
+
+        public static Intersect Ray (double x, double y, double angle, int cellsize, Func<int, int, int> get_cell, double dist = 1000)
+        {
+            ////////////////////////
+            // check input values //
+            ////////////////////////
+
+            //min view dist
+            dist = dist < 1 ? 1 : dist;
+
+            //min size for cells (i.e. ONE cell contains ONE column by default)
+            cellsize = cellsize < 1 ? 1 : cellsize;
+
+            //always filled cell by default
+            if (get_cell == null)
+                get_cell = (cellx, celly) => 1;
+
+            //////////////////
+            // calcualtions //
+            //////////////////
+
+            //pre calc
+            double cosa = Math.Cos(angle);
+            double sina = Math.Sin(angle);
+
+            //ray endpoints
+            double x1 = x;
+            double y1 = y;
+            double x2 = x + dist * cosa;
+            double y2 = y + dist * sina;
+
+            //cell
+            int cx = (int)Math.Ceiling(x / cellsize);
+            int cy = (int)Math.Ceiling(y / cellsize);
+
+            //cell increment
+            int cix = x2 - x1 > 0 ? 1 : -1;
+            int ciy = y2 - y1 > 0 ? 1 : -1;
+
+
+            //...
+
+            /////////////
+            // tracing //
+            /////////////
+
+            //run ray trace
+            Intersect intersect = new Intersect();
+            //...
+            return intersect;
+        }
+    }
+
+    public struct Intersect
+    {
+        public int cell;
+        public int cellx;
+        public int celly;
+        public int column;
+        public bool horizontal;
     }
 }
