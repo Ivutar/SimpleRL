b0c690b1aedc07ddbbc1822a55029dd446a5def7
Ivutar <atrusov@gmail.com>
2016-04-25 18:53:22 +0400

    finished ray tracing

--- src/RL/Examples/E2M4/RayTracer.cs
+++ src/RL/Examples/E2M4/RayTracer.cs
@@ -9,73 +9,11 @@ namespace E2M4
     public static class RayTracer
     {        
 
-        public static Intersect Trace(double x, double y, double angle, int cellsize, Func<int, int, bool> filled_cell, double dist = 1000)
-        {
-            //min view dist
-            dist = dist < 1 ? 1 : dist;
-
-            //min size for cells (i.e. ONE cell contains ONE column by default)
-            cellsize = cellsize < 1 ? 1 : cellsize;
-
-            //always filled cell by default
-            if (filled_cell == null)
-                filled_cell = (cellx, celly) => true;
-
-            //end point
-            double ex = x + dist * Math.Cos(angle);
-            double ey = y + dist * Math.Sin(angle);
-
-            //cell increment
-            int stepX = ex - x > 0 ? 1 : -1;
-            int stepY = ey - y > 0 ? 1 : -1;
-
-            //first intersect
-            double tMaxX = 0;
-            double tMaxY = 0;
-
-            //delta
-            double tDeltaX = (ex - x) / (ey - y);
-            double tDeltaY = (ey - y) / (ex - x);
-
-            //traversal algorithm
-            int cx = (int)Math.Floor(x);
-            int cy = (int)Math.Floor(y);
-            Intersect intersect = new Intersect();
-            do
-            {
-                if (tMaxX < tMaxY)
-                {
-                    intersect.cellx = cx;
-                    intersect.celly = cy;
-                    intersect.horizontal = true;
-                    intersect.column = (int)(tMaxX = Math.Truncate(tMaxX));
-
-                    tMaxX = tMaxX + tDeltaX;
-                    cx = cx + stepX;
-                }
-                else
-                {
-                    intersect.cellx = cx;
-                    intersect.celly = cy;
-                    intersect.horizontal = false;
-                    intersect.column = (int)(tMaxY = Math.Truncate(tMaxY));
-
-                    tMaxY = tMaxY + tDeltaY;
-                    cy = cy + stepY;
-                }
-            } while (!filled_cell(cx, cy));
-
-            return intersect;
-        }
-
-        public static Intersect Ray (double x, double y, double angle, int cellsize, Func<int, int, int> get_cell, double dist = 1000)
+        public static Intersect Ray (double x, double y, double angle, int cellsize, Func<int, int, int> get_cell)
         {
             ////////////////////////
             // check input values //
             ////////////////////////
-
-            //min view dist
-            dist = dist < 1 ? 1 : dist;
 
             //min size for cells (i.e. ONE cell contains ONE column by default)
             cellsize = cellsize < 1 ? 1 : cellsize;
@@ -88,34 +26,103 @@ namespace E2M4
             // calcualtions //
             //////////////////
 
-            //pre calc
-            double cosa = Math.Cos(angle);
-            double sina = Math.Sin(angle);
-
             //ray endpoints
             double x1 = x;
             double y1 = y;
-            double x2 = x + dist * cosa;
-            double y2 = y + dist * sina;
+            double x2 = x + Math.Cos(angle);
+            double y2 = y + Math.Sin(angle);
 
-            //cell
-            int cx = (int)Math.Ceiling(x / cellsize);
-            int cy = (int)Math.Ceiling(y / cellsize);
+            //current cell (zero-based index)
+            int cx = (int)Math.Floor(x / cellsize);
+            int cy = (int)Math.Floor(y / cellsize);
 
-            //cell increment
-            int cix = x2 - x1 > 0 ? 1 : -1;
-            int ciy = y2 - y1 > 0 ? 1 : -1;
+            //increments for cell
+            int icx = x2 - x1 > 0 ? 1 : -1;
+            int icy = y2 - y1 > 0 ? 1 : -1;
 
+            //Ax+By+C=0
+            double A = y1 - y2;
+            double B = x2 - x1;
+            double C = -A * x1 - B * y1;
 
-            //...
+            //x=V, y=H
+            double V = x2 > x1 ? (cx + 1) * cellsize : cx * cellsize;
+            double H = y2 > y1 ? (cy + 1) * cellsize : cy * cellsize;
+
+            //first vertical intersect
+            //{x=V
+            //{Ax+By+C=0 -> AV+By+C=0 -> y=(-C-AV)/B
+            double vx = V;
+            double vy = B != 0 ? -(C + A* V) / B : icy * 1000000 * cellsize; //avoid division by zero
+            double vl = Math.Sqrt((vx - x1) * (vx - x1) + (vy - y1) * (vy - y1)); //length (x1,y1) - (vx,vy)
+
+            //first horizontal intersect
+            //{y=H
+            //{Ax+By+C=0 -> Ax+BH+C=0 -> x = (-C-BH)/A
+            double hy = H;
+            double hx = A != 0 ? -(C + B * H) / A : icx * 1000000 * cellsize; //avoid division by zero
+            double hl = Math.Sqrt((hx - x1) * (hx - x1) + (hy - y1) * (hy - y1)); //length (x1,y1) - (hx,hy)
+
+            //vertical intersection increments
+            double ivx = cellsize * icx;
+            double ivy = x2 != x1 ? cellsize * (y2 - y1) / (x2 - x1) : icy * 1000000 * cellsize; //avoid division by zero
+            double ivl = Math.Sqrt(ivx * ivx + ivy * ivy);
+
+            //horizontal intersection increments
+            double ihx = y2 != y1 ? cellsize * (x2 - x1) / (y2 - y1) : icx * 1000000 * cellsize; //avoid division by zero
+            double ihy = cellsize * icy;
+            double ihl = Math.Sqrt(ihx * ihx + ihy * ihy);
 
             /////////////
             // tracing //
             /////////////
 
-            //run ray trace
+            int c;
             Intersect intersect = new Intersect();
-            //...
+
+            int i = 0;
+            while (++i < 999999) //avoid endless loop
+                if (vl < hl)
+                {
+                    //check vertical intersection
+                    if ((c = get_cell(cx + icx, cy)) > 0)
+                    {
+                        intersect.cell = c;
+                        intersect.cellx = cx + icx;
+                        intersect.celly = cy;
+                        intersect.column = (int)Math.Truncate(Math.IEEERemainder(vy, cellsize));
+                        intersect.distance = vl;
+                        intersect.horizontal = false;
+                        break;
+                    }
+
+                    //do increment
+                    cx += icx;
+                    vx += ivx;
+                    vy += ivy;
+                    vl += ivl;
+                }
+                else
+                {
+                    //check horizontal intersection
+                    if ((c = get_cell(cx, cy + icy)) > 0)
+                    {
+                        intersect.cell = c;
+                        intersect.cellx = cx;
+                        intersect.celly = cy + icy;
+                        intersect.column = (int)Math.Truncate(Math.IEEERemainder(hx, cellsize));
+                        intersect.distance = hl;
+                        intersect.horizontal = true;
+                        break;
+                    }
+
+                    //do increment
+                    cy += icy;
+                    hx += ihx;
+                    hy += ihy;
+                    hl += ihl;
+                }
+
             return intersect;
         }
     }
@@ -126,6 +133,7 @@ namespace E2M4
         public int cellx;
         public int celly;
         public int column;
+        public double distance;
         public bool horizontal;
     }
 }
