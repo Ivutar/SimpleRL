7e543e4b221ca008a09941c26034f2f98efeeb4e
Ivutar <ast@htc-cs.com>
2016-06-01 16:07:11 +0400

    Add Example E2M5 - FOV  (create project)

---
+++ src/RL/Examples/E2M5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E2M5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E2M5 - FOV";
+            Util.Width = 80;
+            Util.Height = 40;
+            Util.CursorVisible = false;
+
+            Util.Buffer.Clear();
+            Util.Buffer.Write(2, 2, "Field Of View - example");
+            Util.Swap();
+
+            Console.ReadKey(true);
+        }
+    }
+}
