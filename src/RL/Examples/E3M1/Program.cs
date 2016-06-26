1df89ccbb4e8da311c163739d85bc2de719ae2c7
Ivutar <atrusov@gmail.com>
2016-06-26 22:18:23 +0400

    E3M1 + RL.UI - create projects
    initial projects creation

---
+++ src/RL/Examples/E3M1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E3M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E3M1 - Simple menu";
+            Util.Width = 80;
+            Util.Height = 25;
+            Util.CursorVisible = false;
+
+            Util.Buffer.Clear();
+            Util.Buffer.Write(0, 0, "Simple menu");
+            Util.Swap();
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ src/RL/RL.UI/Widget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL.UI
+{
+    public class Widget : Canvas
+    {
+        /// <summary>
+        /// Performs event processing and redraws widget if it is required
+        /// </summary>
+        /// <param name="e">event for widget processing</param>
+        /// <returns>returns true - if event was processed and widget was redrawn</returns>
+        public virtual bool ProcessEvent(Event e) { return false; }
+    }
+}
