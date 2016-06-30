2302e42952774421cb30710a61651d0f2cfab38d
Ivutar <atrusov@gmail.com>
2016-06-30 22:38:26 +0400

    UI - created menu class

---
+++ src/RL/Examples/E3M4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E3M4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E3M3 - Menu";
+            Util.Width = 80;
+            Util.Height = 25;
+            Util.CursorVisible = false;
+
+            Util.Buffer.Clear();
+            Util.Buffer.Write(1, 1, "Menu");
+            Util.Swap();
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ src/RL/RL.UI/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL
+{
+    public class Menu : Widget
+    {
+        public List<string> Items { get; private set; }
+
+        public Action<Menu, string, int> OnSelect;
+
+        public Menu (int width, int height, IEnumerable<string> items): base (width, height)
+        {
+            Items = new List<string>(items);
+        }
+
+        public override bool Input(Event e)
+        {
+            //...
+            return false;
+        }
+
+        public override void Draw()
+        {
+            //...
+        }
+    }
+}
