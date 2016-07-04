997655c612260443a4f4aa73d925291ad0a5c2a8
Ivutar <ast@htc-cs.com>
2016-07-04 14:50:06 +0400

    E4M1 - cave generation util
    cave generation util (interface not yet ready)

---
+++ src/RL/Examples/E4M1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E4M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int smoth = 2;
+            int width = 80;
+            int height = 25;
+            
+            Util.Title = "E4M1 - Cellular cave generation";
+            Util.Width = width;
+            Util.Height = height;
+            Util.CursorVisible = false;
+
+            CaveGenerator gen = new CaveGenerator(width, height, 0.4, 5, 3, smoth);
+
+            while(true)
+            {
+                //draw
+                for (int x = 0; x < gen.Width; x++)
+                    for (int y = 0; y < gen.Height; y++)
+                        Util.Buffer.Write(x, y, gen[x, y] == 1 ? "#" : ".");
+                Util.Buffer.Write(1, 1, string.Format(" Seed: {0} ", gen.Seed), Color.LightGreen, Color.Teal);
+                Util.Swap();
+
+                //input
+                Event e = Events.GetNext(true);
+                if (e.Kind == EventKind.Key && e.Key.Press)
+                {
+                    if (e.Key.Key == ConsoleKey.Escape)
+                        break;
+                    else
+                    {
+                        gen.Seed = new Random().Next();
+                        gen.Start(smoth);                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/RL/Examples/E4M1/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E4M1
+{
+    public class Settings : Panel
+    {
+        //...
+    }
+}
