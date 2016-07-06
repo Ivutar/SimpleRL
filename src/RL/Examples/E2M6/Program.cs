14e789c1468b111ff8df6a779f190560b1ed1612
Ivutar <ast@htc-cs.com>
2016-07-06 09:34:03 +0400

    E2M6 - Kobolds Cave
    create empty project for game "Kobolds Cave" and fixed ?. for older
    versions of .Net

---
+++ src/RL/Examples/E2M6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E2M6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E2M6 - Kobolds Cave";
+            Util.Width = 80;
+            Util.Height = 25;
+            Util.CursorVisible = false;
+
+            while (true)
+            {
+                //process input events
+                Event e = Events.GetNext(true);
+                if (e.Kind == EventKind.Key && e.Key.Key == ConsoleKey.Escape)
+                    break;
+
+                //update screen
+                Util.Buffer.Clear();
+                Util.Buffer.Write(0, 0, e.ToString());
+                Util.Swap();
+            }
+        }
+    }
+}
