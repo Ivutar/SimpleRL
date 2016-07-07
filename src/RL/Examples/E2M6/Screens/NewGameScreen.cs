a47f1d640453355ee3654f34273c13a6f4bdfcf4
Ivutar <ast@htc-cs.com>
2016-07-07 10:01:14 +0400

    fill new game screen

--- src/RL/Examples/E2M6/Screens/NewGameScreen.cs
+++ src/RL/Examples/E2M6/Screens/NewGameScreen.cs
@@ -9,9 +9,50 @@ namespace E2M6.Screens
 {
     class NewGameScreen : Screen
     {
+        int current = 0;
+        Menu[] menus;
+        Menu cave_size_menu;
+        Menu popuplation_menu;
+        Menu level_menu;
+        Menu class_menu;
+
         public NewGameScreen ()
         {
-            //...
+            int wd = Util.Buffer.Width - 2;
+
+            //cave size
+            cave_size_menu = new Menu(wd, 1, new string[] {
+                "   Tiny   ",
+                "   Small  ",
+                "  Medium  ",
+                "   Large  ",
+                "   Huge   " })
+            { Horizontal = true, Focus = true };
+
+            //cave population
+            popuplation_menu = new Menu(wd, 1, new string[] {
+                "   Small  ",
+                "  Medium  ",
+                "   Large  "}) { Horizontal = true, Focus = true };
+
+            //starting level
+            level_menu = new Menu(wd, 1, new string[] {
+                "   1 lvl  ",
+                "   5 lvl  ",
+                "  10 lvl  ",
+                "  15 lvl  ",
+                "  20 lvl  " }) { Horizontal = true, Focus = true };
+
+            //hero class
+            class_menu = new Menu(wd, 1, new string[] {
+                "  Peasant ",
+                "   Rogue  ",
+                "  Warrior ",
+                "  Archer  ",
+                "   Mage   " }) { Horizontal = true, Focus = true };
+
+            menus = new Menu[] { cave_size_menu, popuplation_menu, level_menu, class_menu };
+            UpdateMenuColors();
         }
 
         public override void Input(Event e)
@@ -27,8 +68,20 @@ namespace E2M6.Screens
                     //... init new game
                     Global.CurrentScreen = Global.Explore;
                 }
-
-                //...
+                else if (e.Key.Key == ConsoleKey.UpArrow)
+                {
+                    current = current <= 0 ? menus.Length - 1 : current - 1;
+                    UpdateMenuColors();
+                }
+                else if (e.Key.Key == ConsoleKey.DownArrow)
+                {
+                    current = current >= menus.Length - 1 ? 0 : current + 1;
+                    UpdateMenuColors();
+                }
+                else
+                {
+                    menus[current].Input(e);
+                }
             }
         }
 
@@ -36,7 +89,26 @@ namespace E2M6.Screens
         {
             Util.Buffer.Clear();
 
-            //...
+            //menu
+            Color title = Color.Heaven;
+            int menuwd = 5 * 10;
+            int menuhg = menus.Length * 4;
+            int left = (Util.Buffer.Width - menuwd) / 2;
+            int top = (Util.Buffer.Height - menuhg) / 2;
+            cave_size_menu.Draw();
+            popuplation_menu.Draw();
+            level_menu.Draw();
+            class_menu.Draw();
+
+            Util.Buffer.Write(left - 1, top + 4 * 0 - 2, "Cave size",       title);
+            Util.Buffer.Write(left - 1, top + 4 * 1 - 2, "Cave population", title);
+            Util.Buffer.Write(left - 1, top + 4 * 2 - 2, "Starting level",  title);
+            Util.Buffer.Write(left - 1, top + 4 * 3 - 2, "Hero class",      title);
+
+            Util.Buffer.Copy(cave_size_menu,   left, top + 4 * 0);
+            Util.Buffer.Copy(popuplation_menu, left, top + 4 * 1);
+            Util.Buffer.Copy(level_menu,       left, top + 4 * 2);
+            Util.Buffer.Copy(class_menu,       left, top + 4 * 3);
 
             //help
             CharInfo[] help = "{key}ESC{/}: main menu   {key}←↑↓→{/}: change   {key}Enter{/}: start new game".Decorate(Global.decor, Color.LightGray, Color.Black);
@@ -46,5 +118,18 @@ namespace E2M6.Screens
 
             Util.Swap();
         }
+
+        void UpdateMenuColors()
+        {
+            for (int i=0; i<menus.Length; i++)
+            {
+                menus[i].Fore = Color.DarkGray;
+                menus[i].CurrentBack = Color.DarkGray;
+            }
+
+            menus[current].Fore = Color.White;
+            menus[current].CurrentBack = Color.White;
+        }
+
     }
 }
