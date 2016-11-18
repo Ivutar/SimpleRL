099298124ba6bb245e4367cb8b36072d0638afc6
Ivutar <ast@htc-cs.com>
2016-11-18 16:33:51 +0400

    added E2M7 example and SharpNoise package

---
+++ src/RL/Examples/E2M7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RL;
+
+namespace E2M7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Title = "E2M7 - Infinity Map";
+            Util.Width = 80;
+            Util.Height = 25;
+            Util.CursorVisible = false;
+
+            //main loop
+            //while (Global.Sys.Play)
+            //{
+            //    Global.Sys.CurrentScreen.Draw();
+            //    Global.Sys.CurrentScreen.Input(Events.GetNext(true));
+            //}
+        }
+    }
+}
